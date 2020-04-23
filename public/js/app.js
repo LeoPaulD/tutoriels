3f0ab5d573c3088872181554c8cf284335959d01
Léo-Paul Dubourg <leopauldubourg@gmail.com>
2020-04-23 23:30:39 +0200

    filtre + flex design

--- public/js/app.js
+++ public/js/app.js
@@ -49661,8 +49661,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/leo-paul/Images/outil-com-luttes/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/leo-paul/Images/outil-com-luttes/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/leo-paul/Images/tutoriels/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/leo-paul/Images/tutoriels/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/tutoriels/index.blade.php
+++ resources/views/tutoriels/index.blade.php
@@ -6,16 +6,51 @@
 
 @section('content')
 <div di>
-<div class="d-flex justify-content-center" >
-    <div class="row col-2 ">
+<div class="d-flex justify-content-center flex-wrap" >
+    <div class="row col-md-4 col-sm-10 mb-3 col-lg-3 col-xl-3">
             <div class="col-md-12">
                     <div class="card">
-        <div class="card-header">Filtres</div>
+                        <div class="card-header">Filtres</div>
+                        <div class="card-body">
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck1" >
+                                <label class="custom-control-label" for="customCheck1">Audiovisuel</label>
+                            </div>
+                            
+                            
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck3" >
+                                <label class="custom-control-label" for="customCheck3">Web</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck4" >
+                                <label class="custom-control-label" for="customCheck4">Photo</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck5" >
+                                <label class="custom-control-label" for="customCheck5">Illustration</label>
+                            </div>
+                            <div class="custom-control custom-checkbox">
+                                <input type="checkbox" class="custom-control-input" id="customCheck6" >
+                                <label class="custom-control-label" for="customCheck6">Communication</label>
+                            </div>
+                            {{-- <br>
+                            <ul>
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck11" >
+                                    <label class="custom-control-label" for="customCheck11">Culture</label>
+                                </div> 
+                                <div class="custom-control custom-checkbox">
+                                    <input type="checkbox" class="custom-control-input" id="customCheck12" >
+                                    <label class="custom-control-label" for="customCheck12">Tutoriels</label>
+                                </div> 
+                            </ul> --}}
+                        </div>
                     </div>
             </div>
 
     </div>
-    <div class="row  col-9">
+    <div class="row  col-md-8 col-sm-10 col-lg-9 col-xl-7" >
         <div class="col-md-12">
             <div class="card">
                 <div class="card-header">Les Tutoriels</div>
